a04f3848e95ef04b8171e93d428d9fe6ac3cb4b4
ghosttim <adminix@bk.ru>
2015-10-02 12:12:50 +0300

    финал

--- 3/2/server.js
+++ 3/2/server.js
@@ -3,22 +3,6 @@ var req = require('request');
 var url = require('url');
 var pathname = 'https://translate.yandex.net/api/v1.5/tr.json/translate?';
 
-/*
-function res(){
-request('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20151001T164023Z.c33ff54dc6316afc.b0b9dfb6907cb412eb2eead681cb41a6b6141e1c&lang=en-ru&text=hi', function(error, response, body){
-        if(!error && response.statusCode == 200){
-            var result = body{text};
-            console.log(result);
-            return result;
-        }else{
-            console.error(error);
-        }
-    });
-
-}
-console.log(res);
-*/
-
  var body = '<html>'+
     '<head>'+
     '<title>Переводчик</title>'+
@@ -39,7 +23,6 @@ console.log(res);
 
 function onRequest(request, response){
     pathname = pathname + url.parse(request.url).query;
-//console.log(request.url);
         req(pathname, function(error, response, body){
             if(!error && response.statusCode == 200){
                 var result = body;   
@@ -57,9 +40,3 @@ function onRequest(request, response){
 http.createServer(onRequest).listen(8888);
 console.log("Server has started");
 
-/*
-var params = url.parse(pathname, true
-);
-
-    console.dir(params);
-*/
