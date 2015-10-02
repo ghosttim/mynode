ea46763851af1197e71bebe6b6e71b8af97ede17
ghosttim <adminix@bk.ru>
2015-10-02 10:52:55 +0300

    Создан сервер подающий запрос на яндек, получающий ответ, и выводит в консоль

--- 3/2/server.js
+++ 3/2/server.js
@@ -1,5 +1,23 @@
 var http = require('http');
-var urlutils = require('url');
+var req = require('request');
+var url = require('url');
+var pathname = 'https://translate.yandex.net/api/v1.5/tr.json/translate?';
+
+/*
+function res(){
+request('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20151001T164023Z.c33ff54dc6316afc.b0b9dfb6907cb412eb2eead681cb41a6b6141e1c&lang=en-ru&text=hi', function(error, response, body){
+        if(!error && response.statusCode == 200){
+            var result = body{text};
+            console.log(result);
+            return result;
+        }else{
+            console.error(error);
+        }
+    });
+
+}
+console.log(res);
+*/
 
  var body = '<html>'+
     '<head>'+
@@ -8,18 +26,29 @@ var urlutils = require('url');
     'charset=UTF-8" />'+
     '</head>'+
     '<body>'+
-    '<form action="https://translate.yandex.net/api/v1.5/tr.json/translate" method="get">'+
+    '<form action="" method="get" style="text-align:center">'+
     '<input type="text" value="trnsl.1.1.20151001T164023Z.c33ff54dc6316afc.b0b9dfb6907cb412eb2eead681cb41a6b6141e1c" name="key" hidden>'+
     '<input type="radio" name="lang" value="ru-en" checked>Русский - Английский'+
-    '<input type="radio" name="lang" value="en-ru">Английский - Русский<br>'+
-    '<textarea name="text" rows="20" cols="40"></textarea>'+
+    '<input type="radio" name="lang" value="en-ru">Английский - Русский<br><br>'+
+    '<textarea name="text" rows="20" cols="40"></textarea><br><br>'+
     '<input type="submit" value="Перевести" />'+
     '</form>'+
     '</body>'+
     '</html>';
-
+ 
 
 function onRequest(request, response){
+    pathname = pathname + url.parse(request.url).query;
+//console.log(request.url);
+        req(pathname, function(error, response, body){
+            if(!error && response.statusCode == 200){
+                var result = body;   
+                console.log(result);
+                return result;
+            }else{
+                console.error("Ошибка! "+ error);
+            }
+        });
 	response.writeHead(200, {"Content-Type":"text/html"});
 	response.write(body);
 	response.end();
@@ -27,3 +56,10 @@ function onRequest(request, response){
 
 http.createServer(onRequest).listen(8888);
 console.log("Server has started");
+
+/*
+var params = url.parse(pathname, true
+);
+
+    console.dir(params);
+*/
