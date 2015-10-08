949f4e1b0492ec8a7f295d84db90e0bd5ae4ec49
ghosttim <adminix@bk.ru>
2015-10-08 16:31:24 +0300

    tasks

---
+++ 5/models/tasks.js
@@ -0,0 +1,18 @@
+var Tasks = {
+	list: function(calback){
+
+	},
+	add: function(task, calback){
+
+	},
+	change: function(id, text, calback){
+
+	},
+	complete: function(id, calback){
+
+	},
+	delete: function(id, calback){
+
+	}
+};
+module.exports = Tasks;
