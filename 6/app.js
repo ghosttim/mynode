77aa5bf9748461d8681feb9a631dfeb04d595117
ghosttim <adminix@bk.ru>
2015-10-15 16:36:39 +0300

    добавленны куки

--- 6/app.js
+++ 6/app.js
@@ -10,18 +10,28 @@ app.use(bodyParser());
 var session = require('cookie-session');
 app.use(session({keys: ['secret']}));
 
-app.set('trust proxy', 1);
+//app.set('trust proxy', 1);
 
 var pars = bodyParser.urlencoded({extended: false});
 
 
 
 app.get("/",function(req,res){
-	if(req.session.auth == 'admin') {
-		res.send(200, "<form action='/logout' method='get'><input type='submit' value='Logout'></form>");
 
-	}else {
-       res.send(200,"<form action='/login' method='post'><input type='text' name='username' placeholder='Login'><br><input type='password' name='pass' placeholder='Password'><br>Запомнить меня<input type='checkbox' name='save'><input type='submit' value='Login'></form><br>");
+   if(req.session.auth == 'admin') {
+		res.send(200, "<form action='/logout' method='get'><input type='submit' value='Logout'></form><br><br><a href='/topsecret'>Top Secret</a>");
+
+
+	}else{
+
+		 if(req.cookies.uLogin == "admin" && req.cookies.uPass == "admin"){
+         req.session.auth = 'admin';
+         res.redirect('/');
+         
+
+	}else{
+       res.send(200,"<form action='/login' method='post'><input type='text' name='username' placeholder='Login'><br><input type='password' name='pass' placeholder='Password'><br>Remember me<input type='checkbox' name='save'><input type='submit' value='Login'></form><br>");
+	     }
 	}
 	
 });
@@ -32,43 +42,42 @@ app.post("/login", pars, function(req, res){
 	}else{
 		uLogin = req.body.username;
         uPass  = req.body.pass;
-		//res.send(200, uLogin+ "  "+ uPass);
+        uSave  = req.body.save;
+  
 		if (uLogin == "admin" && uPass == "admin") {
 			req.session.auth = 'admin';
 
-			res.cookie('uLogin',uLogin);
+           if (uSave){
+           	res.cookie('uLogin',uLogin);
             res.cookie('uPass', uPass);
+           }
+			
 
 
-			res.end(req.session.auth);
-
+			//res.write("Welcome, you are logged in as: " +req.session.auth+"<br><br><a href='/'>Home Page</a><br><a href='/topsecret'>Top Secret</a>");
+            res.redirect('/')
 		}else{
-		res.send("Bad login or password");
+		res.send("Incorrect login or password<br><br><a href='/'>Back</a>");
 	    }
 	}
 });
 
-app.get("/test", function(req, res){
+app.get("/topsecret", function(req, res){
 	//res.send(200, "кто здесь  " +req.session.auth);
 	if(req.session.auth != 'admin'){
 		res.redirect('/');
 	}else {
-		res.send(200, "welcome");
+		res.send(200, "Welcome to classified information.<br><br><a href='/'>Home Page</a>");
 	}
 });
 
 app.get("/logout",function(req, res){
 	req.session.auth = null;
+	res.clearCookie('uLogin');
+	res.clearCookie('uPass');
 	res.redirect('/');
 
 });
-
-
-
-
-
-
-
 
 
 
