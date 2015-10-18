7d6625f9a6b8f5888435b048950fc93cc5ca7c80
Artem <adminix@bk.ru>
2015-10-18 21:59:46 +0300

    rest api google maps

---
+++ 7/rest.js
@@ -0,0 +1,15 @@
+var util    = require('util');
+var restify = require('restify');
+
+var client = restify.createJsonClient({
+    url: "http://maps.google.com/"
+});
+
+client.get('/maps/api/geocode/json?address=Mosscow+Red+Square', function(err, req, res, obj){
+    if(err)
+        console.error(err);
+    else
+       // console.log(util.inspect(obj, {depth: null}));
+        console.log(obj.results[0].formatted_address);
+        // video 0:34
+});
